44c57cee07ec293d1c93dac9f2f4f11b9771a297
http://jneen.net/ <jneen@jneen.net>
2018-03-29 14:56:27 +0900

    gh-pages update

---
+++ javascripts/reveal-config.js
@@ -0,0 +1,12 @@
+(function() {
+  Reveal.initialize({
+    slideNumber: true,
+    width: 1600,
+    height: 900,
+    history: true,
+    keyboard: true,
+    transition: 'linear',
+    transitionSpeed: 'fast'
+  });
+
+}).call(this);
